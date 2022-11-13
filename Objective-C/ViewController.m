484c5219dab43b9affb405fd1427c0a75acd95ee
AndreyBP <an.balobanov@gmail.com>
2022-11-13 16:45:33 +0300

    vers 1

--- Objective-C/ViewController.m
+++ Objective-C/ViewController.m
@@ -8,6 +8,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+
+//привязка outlet Label и TextField
 @property (weak, nonatomic) IBOutlet UILabel *arrayNameLabel;
 @property (weak, nonatomic) IBOutlet UITextField *addArrayNameTextField;
 
@@ -16,30 +18,35 @@
 
 @end
 
+
 @implementation ViewController
-NSMutableArray *arrayName;
-NSString *name;
+
+NSMutableArray *arrayName; //объявление изменяемого массива
+NSString *name; //объявление текстовой переменной
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    arrayName = [[NSMutableArray alloc] init];
+    arrayName = [[NSMutableArray alloc] init]; // загрузка изменяемого массива
 }
 
+// кнопка перечисления слов через запятую в TextField
 - (IBAction)saveNameArrayButton:(id)sender {
     name = self.addArrayNameTextField.text;
     [arrayName addObject: [NSString stringWithString: name]];
     self.arrayNameLabel.text = [arrayName componentsJoinedByString: @", "];
 }
 
+
+//возведение числа в степень и появление в TextField
 - (IBAction)resultDegreeTwo:(id)sender {
     
-    NSString *numberD = self.resultTextField.text;
-    NSCharacterSet *charSet = [NSCharacterSet characterSetWithCharactersInString: numberD];
+    NSString *number = self.resultTextField.text;
+    NSCharacterSet *charSet = [NSCharacterSet characterSetWithCharactersInString: number];
     NSCharacterSet *numSet = [NSCharacterSet decimalDigitCharacterSet];
     
     if([numSet isSupersetOfSet: charSet])
     {
-        int myInt = (int)round( [numberD intValue]);
+        int myInt = (int)round( [number intValue]);
         self.numberLabel.text = [@(pow(myInt,2)) description]; }
     else {
         self.numberLabel.text = @"введите целое число";
